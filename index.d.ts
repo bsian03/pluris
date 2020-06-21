a4acfd4206b49515a6e2121fc3dc3d1d5a48cd29
Bsian <bsian@staff.libraryofcode.us>
2020-06-21 22:49:40 +0100

    Unfortunately ts is an ass and won't override eris docs

--- index.d.ts
+++ index.d.ts
@@ -1,80 +1,58 @@
 import { EventEmitter } from 'events';
-import * as eris from 'eris';
 
-declare global {
-  namespace Eris {
-    interface CollectorOptions {
-      timeout?: number;
-      count?: number;
-    }
-
-    interface MessageCollectorOptions extends CollectorOptions {
-      filter?: (msg: eris.Message) => boolean;
-    }
-
-    interface ReactionCollectorOptions extends CollectorOptions {
-      filter?: (userID: string) => boolean;
-    }
-
-    interface CollectedReaction<T extends Textable = eris.TextableChannel> {
-      msg: eris.Message<T>;
-      emoji: Emoji;
-      userID: string;
-    }
-
-    class MessageCollector<T extends Textable = eris.TextableChannel> extends EventEmitter implements MessageCollectorOptions {
-      channel: T;
-      timeout: number;
-      count: number;
-      filter: (msg: Message) => boolean;
-      collected: eris.Collection<Message<T>>;
-      running: boolean;
-      constructor(channel: T, options?: MessageCollectorOptions);
-      run(): Promise<MessageCollector<T>>;
-      stop(): void;
-      on(listener: 'collect' | 'update' | 'delete', event: (msg: Message) => void): this;
-      on(listener: 'stop', event: () => void): this;
-    }
-
-    class ReactionCollector<T extends Textable = eris.TextableChannel> extends EventEmitter implements ReactionCollectorOptions {
-      message: eris.Message<T>;
-      timeout: number;
-      count: number;
-      filter: (userID: string) => boolean;
-      collected: CollectedReaction<T>[];
-      running: boolean;
-      run(): Promise<ReactionCollector<T>>;
-      stop(): void;
-      on(listener: 'collect', event: (x: CollectedReaction) => void): this;
-    }
-
-    class Message<T extends Textable = eris.TextableChannel> {
-      awaitReactions(options: ReactionCollectorOptions): ReactionCollector<T>;
-    }
-
-    class TextChannel {
-      awaitMessages(options: MessageCollectorOptions): Promise<MessageCollector<TextChannel>>;
-    }
-    class PrivateChannel {
-      awaitMessages(options: MessageCollectorOptions): Promise<MessageCollector<PrivateChannel>>;
-    }
-
-    interface Textable {
-      awaitMessages(options: MessageCollectorOptions): Promise<MessageCollector>
-    }
-    interface GuildTextable {
-      awaitMessages(options: MessageCollectorOptions): Promise<MessageCollector<eris.GuildTextableChannel>>
-    }
-
-    class Client {
-      awaitMessageReactions(message: eris.Message, options: ReactionCollectorOptions): Promise<ReactionCollector>;
-      awaitChannelMessages(channel: eris.TextableChannel, options: MessageCollectorOptions): Promise<MessageCollector>;
-    }
+declare module 'eris' {
+  interface CollectorOptions {
+    timeout?: number;
+    count?: number;
   }
-};
+
+  interface MessageCollectorOptions extends CollectorOptions {
+    filter?: (msg: Message) => boolean;
+  }
+
+  interface ReactionCollectorOptions extends CollectorOptions {
+    filter?: (userID: string) => boolean;
+  }
+
+  interface CollectedReaction<T extends Textable = TextableChannel> {
+    msg: Message<T>;
+    emoji: Emoji;
+    userID: string;
+  }
+
+  class MessageCollector<T extends Textable = TextableChannel> extends EventEmitter implements MessageCollectorOptions {
+    channel: T;
+    timeout: number;
+    count: number;
+    filter: (msg: Message) => boolean;
+    collected: Collection<Message<T>>;
+    running: boolean;
+    constructor(channel: T, options?: MessageCollectorOptions);
+    run(): Promise<MessageCollector<T>>;
+    stop(): void;
+    on(listener: 'collect' | 'update' | 'delete', event: (msg: Message) => void): this;
+    on(listener: 'stop', event: () => void): this;
+  }
+
+  class ReactionCollector<T extends Textable = TextableChannel> extends EventEmitter implements ReactionCollectorOptions {
+    message: Message<T>;
+    timeout: number;
+    count: number;
+    filter: (userID: string) => boolean;
+    collected: CollectedReaction<T>[];
+    running: boolean;
+    run(): Promise<ReactionCollector<T>>;
+    stop(): void;
+    on(listener: 'collect', event: (x: CollectedReaction) => void): this;
+  }
+
+  interface Textable {
+    awaitMessages(options: MessageCollectorOptions): Promise<MessageCollector>
+  }
+}
 
 declare type PlurisPlugins = { [plugin: string]: boolean };
 
-declare function Pluris(Eris: eris, options?: PlurisPlugins): void;
+declare function Pluris(Eris: typeof import('eris'), options?: PlurisPlugins): void;
 
 export = Pluris;
