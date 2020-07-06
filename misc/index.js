5d3d4ca0c804a0a48f95a200ce0549e2cb07a453
Bsian <bsian@staff.libraryofcode.us>
2020-07-06 23:04:37 +0100

    Support getters

--- misc/index.js
+++ misc/index.js
@@ -1,23 +1,24 @@
 module.exports = {
   /**
-   * @param {import('eris')} Eris
+   * @param {typeof import('eris')} Eris
    * @param {String} Class
    * @param {Function} fn
+   * @param {Boolean} getter
    */
-  loadPrototype: (Eris, Class, fn) => {
+  loadPrototype: (Eris, Class, fn, getter = false) => {
     if (Eris[Class].prototype[fn.name]) return console.warn(`${fn.name} prototype already exists in ${Class}! ${this.warningMessage('prototype')}`);
     fn = fn.bind(Eris[Class].prototype);
-    Eris[Class].prototype[fn.name] = fn;
+    Object.defineProperty(Eris[Class].prototype, fn.name, getter ? { get: fn } : { value: fn });
     console.log(`Loaded ${Class}#${fn.name}`);
   },
 
   /**
-   * @param {import('eris')} Eris
+   * @param {typeof import('eris')} Eris
    * @param {new (...args) => any} Class
    */
   loadImport: (Eris, Class) => {
     if (Eris[Class.name]) return console.warn(`Eris.${Class.name} is already defined! ${this.warningMessage('import')}`);
-    Eris[Class.name] = Class;
+    Object.defineProperty(Eris, Class.name, { value: Class });
     console.log(`Loaded import ${Class.name}`);
   },
 
